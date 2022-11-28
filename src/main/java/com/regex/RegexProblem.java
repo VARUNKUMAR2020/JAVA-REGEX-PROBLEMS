f6310bbaa3ffbba449ee12498e9435600104f046
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-28 21:47:50 +0530

    Add: Validating First Name

--- src/main/java/com/regex/RegexProblem.java
+++ src/main/java/com/regex/RegexProblem.java
@@ -1,7 +1,27 @@
 package com.regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegexProblem {
+    static final String firstNamePattern = "^[A-Z][a-z]{2,}";
+
+    // UseCase1 : Validate first name.
+    public static void validateFirstName(String firstName) {
+        Pattern pattern = Pattern.compile(firstNamePattern);
+        Matcher matcher = pattern.matcher(firstName);
+        boolean matchFound = matcher.find();
+        if (matchFound)
+            System.out.println("Match Found.");
+        else
+            System.out.println("Match Not Found.");
+    }
+
     public static void main(String[] args) {
         System.out.println("Regex Problems");
+        
+        // UseCase1 : Validate first name.
+        String firstName = "Varun";
+        validateFirstName(firstName);
     }
 }
