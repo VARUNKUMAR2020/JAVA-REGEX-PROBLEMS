c4076dbf5a2e4e73e63790f441c09bee2e75e31e
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-28 21:52:39 +0530

    Add: Validating Last Name

--- src/main/java/com/regex/RegexProblem.java
+++ src/main/java/com/regex/RegexProblem.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 
 public class RegexProblem {
     static final String firstNamePattern = "^[A-Z][a-z]{2,}";
+    static final String lastNamePattern = "^[A-Z][a-z]{2,}";
 
     // UseCase1 : Validate first name.
     public static void validateFirstName(String firstName) {
@@ -12,16 +13,31 @@ public class RegexProblem {
         Matcher matcher = pattern.matcher(firstName);
         boolean matchFound = matcher.find();
         if (matchFound)
-            System.out.println("Match Found.");
+            System.out.println("First Name Match Found.");
         else
-            System.out.println("Match Not Found.");
+            System.out.println("First Name Match Not Found.");
+    }
+
+    // UseCase2 : Validate last name.
+    public static void validateLastName(String lastName) {
+        Pattern pattern = Pattern.compile(lastNamePattern);
+        Matcher matcher = pattern.matcher(lastName);
+        boolean matchFound = matcher.find();
+        if (matchFound)
+            System.out.println("Last Name Match Found.");
+        else
+            System.out.println("Last Name Match Not Found.");
     }
 
     public static void main(String[] args) {
         System.out.println("Regex Problems");
-        
+
         // UseCase1 : Validate first name.
         String firstName = "Varun";
         validateFirstName(firstName);
+
+        // UseCase2 : Validate last name.
+        String lastName = "Kumar";
+        validateLastName(lastName);
     }
 }
